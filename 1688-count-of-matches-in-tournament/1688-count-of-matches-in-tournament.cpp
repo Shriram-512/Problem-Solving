a4e6e8d827860ecb2f6ee85d12c73c2b07c04d14
Shriram <86714137+Shriram-512@users.noreply.github.com>
2022-10-07 22:41:40 +0530

    Time: 3 ms (38.27%), Space: 5.9 MB (23.58%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        // int nmatches=0;
+        // int temp =0;
+        //     if(n%2==0)
+        //     {
+        //         while(n>0)
+        //         {
+        //             nmatches= nmatches + n/2;
+        //             n = n/2;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         while(n>0)
+        //         {
+        //             nmatches = nmatches + (n-1)/2;
+        //             n = n/2;
+        //         }
+        //         n;
+        //     }
+        
+        
+        //sanket approach
+        int match=  0;
+        int tmatch = 0;
+        while(n > 1 )
+        {
+            if(n % 2 == 0)
+            {
+                match =  n / 2;
+                n -= match;   
+            }
+            else if(n % 2 == 1)
+            {
+                match =  (n / 2) + 1;
+                 n -= match;   
+
+            }
+            tmatch += match;
+        }
+        return tmatch;
+    }
+};
