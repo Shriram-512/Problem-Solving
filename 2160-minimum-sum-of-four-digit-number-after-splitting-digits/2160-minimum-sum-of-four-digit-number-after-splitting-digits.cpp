c8014788692bf7ea079f5f7de631190a9072e1ff
Shriram <86714137+Shriram-512@users.noreply.github.com>
2022-10-11 23:42:25 +0530

    Time: 5 ms (16.17%), Space: 6 MB (24.31%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -3,8 +3,8 @@ public:
     int minimumSum(int num) {
         string s = to_string(num);  //int to string conversion
         sort(s.begin(),s.end());  //sorting the string
-        int res = (s[0] - '0') * 10 + s[3] - '0'; //  first * 10 + third
-        res += (s[1] - '0') * 10 + s[2] - '0';   //  second * 10 + fourth
+        int res = (s[0] - '0') * 10 + s[3] - '0'; //  first * 10 + fourth
+        res += (s[1] - '0') * 10 + s[2] - '0';   //  second * 10 + third
         return res;
     }
 };
